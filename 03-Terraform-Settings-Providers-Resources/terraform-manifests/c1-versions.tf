6fecf74d1cc8b89dcdfe7726473c9eff64f00eed
jayamanideep <manideepk736@gmail.com>
2024-07-23 11:53:12 +0530

    Update c1-versions.tf
    first commit

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -11,6 +11,7 @@ terraform {
 # Provider Block
 provider "aws" {
   region = "us-east-1"
+  profile = "ab"
 }
 
 /*
